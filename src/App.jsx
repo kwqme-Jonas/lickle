f2567a9352fc94ec59e6e8c3290af1941dc5e367
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2022-10-30 22:41:12 +0000

    installed some packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <div className="w-screen h-screen flex       items-center justify-center text-red-600"> App
+        </div>
+    );
+};
 
-export default App
+export default App;
