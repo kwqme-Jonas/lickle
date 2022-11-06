b220be66baa6b89be8fe8b46659e61ff50532bf8
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2022-11-06 12:51:31 +0000

    updated header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../img/logo.png';
 import Avatar from '../img/avatar.png';
-import { MdShoppingBasket } from 'react-icons/md';
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md';
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from '../firebase.config';
@@ -17,13 +17,20 @@ const Header = () => {
     const provider = new GoogleAuthProvider();
 
     const [{ user }, dispatch] = useStateValue();
+
+    const [isMenu, setisMenu] = useState(false);
     
     const login = async () => {
-        const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
+        if (!user) {
+            const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
         dispatch({
-            type : action.
+            type : actionType.SET_USER,
             user : providerData[0],
-        }) 
+        });
+        localStorage.setItem('user', JSON.stringify(providerData[0]));
+        } else {
+            setisMenu(!isMenu);
+        }
     };
 
     return (
@@ -45,15 +52,34 @@ const Header = () => {
                 <div className='relative flex items-center justify-center -top-1'>
                     <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
                     <div className=' absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex item-center justify-center'>
-                        <p className='text-sm  text-white font-semibold'>2</p>
+                        <p className='text-sm text-white font-semibold'>2</p>
                      </div>
                     </div>
                     
                     <div className='relative'>
-                    <motion.img whileTap={{scale: 0.6 }}
-                            src={Avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer' alt="userprofile"
+                        <motion.img whileTap={{scale: 0.6 }}
+                            src={user ? user.photoURL : Avatar}
+                            className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' alt="userprofile"
                             onClick={login}
                         />
+                        {
+                            isMenu && (
+                                <motion.div initial={{ opacity: 0, scale: 0.6 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit ={{opacity: 0, scale: 0.6}}
+                                    className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
+                            {user && user.email === 'jokansey@gmail.com' && (
+                                    <Link to={'/createItem'}>
+                                        <p className='px-2 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item<MdAdd />
+                                        </p>
+                                    </Link>
+                                )
+                            }   
+                            <p className='px-2 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>Logout<MdLogout />
+                            </p>
+                            </motion.div>
+                         )
+                        }
                     </div>
                 </div>
             </div>
