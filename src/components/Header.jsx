4e488fb624836200e2971f18513fdbfd1b56882d
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2022-11-13 16:01:54 +0000

    updated homeContainer.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -83,7 +83,9 @@ const Header = () => {
                                     className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
                             {user && user.email === 'jokansey@gmail.com' && (
                                     <Link to={'/createItem'}>
-                                        <p className='px-2 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item<MdAdd />
+                                            <p className='px-2 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                                onClick={() => setisMenu(false)}
+                                            >New Item<MdAdd />
                                         </p>
                                     </Link>
                                 )
@@ -128,17 +130,27 @@ const Header = () => {
                                     className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
                             {user && user.email === 'jokansey@gmail.com' && (
                                     <Link to={'/createItem'}>
-                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item<MdAdd />
+                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                            onClick={() => setisMenu(false)}
+                                        >New Item<MdAdd />
                                         </p>
                                     </Link>
                                 )
                             }   
 
                         <ul  className='flex flex-col'>
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Home</li> 
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Menu</li> 
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li> 
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service</li> 
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                            onClick={() => setisMenu(false)}
+                            >Home</li> 
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                            onClick={() => setisMenu(false)}
+                            >Menu</li> 
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                            onClick={() => setisMenu(false)}
+                            >About Us</li> 
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                            onClick={() => setisMenu(false)}
+                            >Service</li> 
                         </ul>
                                 <p className='m-2 p-2 rounded-md shadow-md flex items-center bg-gray-200 justify-center gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'
                                     onClick={logout}
