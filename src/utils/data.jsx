ff1207c61b5e5e295c630e74f77072348d7d1f86
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2022-11-13 23:35:06 +0000

    made some updates

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -11,4 +11,43 @@ import Fi1 from '../img/fi1.png';
     { id: 3, name: 'Chicken Kebab', decp: 'Mixed kebab', price: '8.25', imageSrc: c3 },
 
     {id : 4, name: 'Fish Kebab', decp:'Mixed Fish Kebab', price: '5.25',imageSrc : Fi1},
-]
+];
+
+export const categories = [
+    {
+      id: 1,
+      name: "Chicken",
+      urlParamName: "chicken",
+    },
+    {
+      id: 2,
+      name: "Curry",
+      urlParamName: "curry",
+    },
+    {
+      id: 3,
+      name: "Rice",
+      urlParamName: "rice",
+    },
+    {
+      id: 4,
+      name: "Fish",
+      urlParamName: "fish",
+    },
+    {
+      id: 5,
+      name: "Fruits",
+      urlParamName: "fruits",
+    },
+    {
+      id: 6,
+      name: "Icecreams",
+      urlParamName: "icecreams",
+    },
+  
+    {
+      id: 7,
+      name: "Soft Drinks",
+      urlParamName: "drinks",
+    },
+  ];
